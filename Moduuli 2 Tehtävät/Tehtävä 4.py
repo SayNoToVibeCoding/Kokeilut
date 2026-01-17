1c658e9aef119e01d7338337cc98b1e7e46fef3e
ronik <ronikais@metropolia.fi>
2026-01-17 17:02:14 +0200

    .

---
+++ Moduuli 2 Tehtävät/Tehtävä 4.py
@@ -0,0 +1,9 @@
+Numero1 = float(input("Numero 1: "))
+Numero2 = float(input("Numero 2: "))
+Numero3 = float(input("Numero 3: "))
+Summa = Numero1 + Numero2 + Numero3
+Kerto = Numero1 * Numero2 * Numero3
+Keski = Summa / 3
+print("Numeroiden summa: " + str(Summa))
+print("Numeroiden tulo: " + str(Kerto))
+print("Numeroiden keskiarvo: " + str(Keski))
