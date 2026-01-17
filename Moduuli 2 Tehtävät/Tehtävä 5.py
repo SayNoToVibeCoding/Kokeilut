ef6458e31dea2e83e362b0cfeef4b478bc47e842
ronik <ronikais@metropolia.fi>
2026-01-17 17:04:59 +0200

    .

--- Moduuli 2 Tehtävät/Tehtävä 5.py
+++ Moduuli 2 Tehtävät/Tehtävä 5.py
@@ -1,14 +1,11 @@
 Leiviskä = float(input("Anna Leiviskät:"))
 Naula = float(input("Anna Naulat:"))
 Luoti = float(input("Anna Luodit:"))
-Luoti = 13.3
+Luodinpaino = 13.3
 NaulatLuoti = 32
 LeiviskäNaula = 20
 Luoteja_yht = Leiviskä * LeiviskäNaula * NaulatLuoti + Naula * NaulatLuoti + Luoti
-massa = Luoteja_yht * Luoti
+massa = Luoteja_yht * Luodinpaino
 kg = float(massa // 1000)
 gramma = massa % 1000
 print(f"Massa Nykymittojen Mukaan: = {kg} kg {gramma:.2f} g")
-
-
-# Arvo jonka antoi oli hieman eri kuin esimerkissä, mutta en ymmärtänyt mistä tämä johtuu.
