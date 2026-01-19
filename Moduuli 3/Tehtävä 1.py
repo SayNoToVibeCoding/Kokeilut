cafe20031dc43c647bd3b5b6843357777856754a
ronik <ronikais@metropolia.fi>
2026-01-19 11:26:31 +0200

    .

--- Moduuli 3/Tehtävä 1.py
+++ Moduuli 3/Tehtävä 1.py
@@ -1,3 +1,3 @@
 KalaP = float(input("Kuhan pituus senttimetrinä: "))
 if KalaP <= 37:
-    print("Kuha on alimittainen " + str(37 - KalaP) + "cm" )
+    print("Kuha on alimittainen " + str(37 - KalaP) + "cm laske se takaisin järveen" )
