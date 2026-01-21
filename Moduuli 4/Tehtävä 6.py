72d69add394539d4f803f9573ce06443bfa4802d
ronik <ronikais@metropolia.fi>
2026-01-21 14:56:19 +0200

    .

--- Moduuli 4/Tehtävä 6.py
+++ Moduuli 4/Tehtävä 6.py
@@ -2,12 +2,13 @@ import random
 
 pisteet = int(input("Anna arvottavien pisteiden määrä: "))
 pMäärä = 0
-silmukat = 0
-while silmukat < pisteet:
+kierrokset = 0
+while kierrokset < pisteet:
     x = random.randint(-1000,1000) / 1000
     y = random.randint(-1000,1000) / 1000
     if x**2 + y**2 < 1:
         pMäärä += 1
-    silmukat += 1
+        #En tiennyt mikä on origo tai Monte Carlon laskukaava
+    kierrokset += 1
 likiarvo = 4 * pMäärä / pisteet
 print("Piin likiarvo on:", likiarvo)
